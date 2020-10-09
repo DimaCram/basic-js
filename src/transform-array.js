dac383d0294cf6d60e5156fec9621c76eef70156
Dmitrij <kramkov@ncpi.gov.by>
2020-10-09 18:24:44 +0300

    Completed

--- src/transform-array.js
+++ src/transform-array.js
@@ -9,16 +9,16 @@ module.exports = function transform(arr) {
   for(let i = 0; i < arr.length; i++){
     switch(arr[i]){
       case "--discard-next":
-          i+=2;
+          i++;
         break;
         case "--discard-prev":
-          if(i !== 0) res.pop();
+          if(i !== 0  && arr[i - 2] !== "--discard-next") res.pop();
         break;
         case "--double-next":
           if(i + 1 < arr.length) res.push(arr[i + 1]);
         break;
         case "--double-prev":
-          if(i - 1 >= 0)
+          if(i - 1 >= 0 && arr[i - 2] !== "--discard-next")
             res.push(res[res.length - 1]);
         break;
         default:
